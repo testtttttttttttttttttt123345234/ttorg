dfa2f81699853a20924b4dbaec7a6f1f48aac70c
KariemAlwazany <kariemalwazany@gmail.com>
2023-08-04 19:51:37 +0300

    fixing bad smellss

--- src/main/java/org/example/loginEntity.java
+++ src/main/java/org/example/loginEntity.java
@@ -1,9 +1,10 @@
 package org.example;
 
 import java.sql.*;
-
+import java.util.logging.*;
 
 public class loginEntity {
+    private static final Logger logger = Logger.getLogger(loginEntity.class.getName());
 
     boolean Owner = false;
     boolean reg = false;
@@ -16,7 +17,7 @@ public class loginEntity {
     String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
     String owner = "owner";
 
-    String Role;
+    String role;
 
     public  String checkValues(String UserName,String Password) {
 
@@ -27,19 +28,17 @@ public class loginEntity {
                 ResultSet resultSet = statement.executeQuery(query);
                 while (resultSet.next()) {
                     flag = 1;
-                    if (resultSet.getString(3).equalsIgnoreCase("tenant")) {Role="tenant";} else if (resultSet.getString(3).equalsIgnoreCase("admin")) {Role="admin";} else if (resultSet.getString(3).equalsIgnoreCase("owner")) {Role="owner";}
+                    if (resultSet.getString(3).equalsIgnoreCase("tenant")) {role="tenant";} else if (resultSet.getString(3).equalsIgnoreCase("admin")) {role="admin";} else if (resultSet.getString(3).equalsIgnoreCase("owner")) {role="owner";}
                 }
                 if (flag == 0) {
-                    Role="null";
-                }return Role;
-        } catch (Exception ex) {}
+                    role="null";}return role;} catch (Exception ex) { logger.info("An exception occurred: " + ex.getMessage());}
         return null;
     }
 
     public boolean printOwner(String fName, String mName, String lName, String Phone, String owEmail, String age, String owUser, String owPass) {
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             Statement statement2 = connection.createStatement();
-            String query2 = "insert into login (username, password, role) values ('"+owUser+"', '"+owPass+"', '"+owner+"')";statement2.executeUpdate(query2);Statement statement3 = connection.createStatement();String query3 = "insert into owner (first_name, second_name, last_name, phonenumber, email, age, username, password, role) values ('"+fName+"', '"+mName+"', '"+lName+"', '"+Phone + "', '"+owEmail+"', '"+age+"', '"+owUser+"', '"+owPass+"', '"+owner+"')";statement3.executeUpdate(query3);Owner = true;} catch (SQLException e) {}
+            String query2 = "insert into login (username, password, role) values ('"+owUser+"', '"+owPass+"', '"+owner+"')";statement2.executeUpdate(query2);Statement statement3 = connection.createStatement();String query3 = "insert into owner (first_name, second_name, last_name, phonenumber, email, age, username, password, role) values ('"+fName+"', '"+mName+"', '"+lName+"', '"+Phone + "', '"+owEmail+"', '"+age+"', '"+owUser+"', '"+owPass+"', '"+owner+"')";statement3.executeUpdate(query3);Owner = true;} catch (SQLException e) {logger.info("An exception occurred: " + e.getMessage());}
         return Owner;
     }
 
@@ -47,8 +46,8 @@ public class loginEntity {
         String tenant = "tenant";
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             Statement statement3 = connection.createStatement();
-            String query3 = "insert into login (username, password, role) values ('"+tenUser+"','"+tenPass+"', '"+tenant+"')";statement3.executeUpdate(query3);Statement statement4 = connection.createStatement();String query4 = "insert into tenant (first_name, second_name, last_name, phonenumber, email, age, reg_num, major, username, password, role) values ('"+ffName+"', '"+mmName+"', '"+llName+"', '"+PPhone+"', '"+tenEmail+"', '"+Age+"', '"+Reg_num+"', '"+major+"', '"+tenUser+"','"+tenPass+"', '"+tenant+"')";statement4.executeUpdate(query4);Tenant = true;} catch (SQLException e) {}
-        return Tenant;
+            String query3 = "insert into login (username, password, role) values ('"+tenUser+"','"+tenPass+"', '"+tenant+"')";statement3.executeUpdate(query3);Statement statement4 = connection.createStatement();String query4 = "insert into tenant (first_name, second_name, last_name, phonenumber, email, age, reg_num, major, username, password, role) values ('"+ffName+"', '"+mmName+"', '"+llName+"', '"+PPhone+"', '"+tenEmail+"', '"+Age+"', '"+Reg_num+"', '"+major+"', '"+tenUser+"','"+tenPass+"', '"+tenant+"')";statement4.executeUpdate(query4);Tenant = true;} catch (SQLException e) {logger.info("An exception occurred: " + e.getMessage());}
+        return true;
     }
 
     public boolean failureReg(String tenUser, String tenPass) {
@@ -60,7 +59,7 @@ public class loginEntity {
             if (resultSet.next()) {
                 reg = true;
             }
-        } catch (SQLException e) {
+        } catch (SQLException e){logger.info("An exception occurred: " + e.getMessage());
         }
         return reg;
     }
